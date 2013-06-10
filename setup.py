793d03451d94a21cb667ca951a4eae7beff8c5aa
Trenton Broughton <trenton@devpie.com>
2013-06-10 12:05:51 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="klein",
-    version="0.1.0",
+    version="0.1.1",
     description="werkzeug + twisted.web",
     packages=["klein"],
     install_requires=["Twisted", "werkzeug", "mock"]
